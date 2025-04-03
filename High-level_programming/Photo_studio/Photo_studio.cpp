960300b155be6633a01eee6d58f01859ac4fa275
afanasevauliana <afanasevauliana946@gmail.com>
2025-04-03 22:10:36 +0300

    lab1

---
+++ High-level_programming/Photo_studio/Photo_studio.cpp
@@ -0,0 +1,38 @@
+#include "menu/CMenu.h"
+#include "menu/CMenuItem.h"
+#include <cmath>
+using namespace std;
+
+
+#pragma region функции-заглушки
+int f1() {
+    int value = 25;
+    cout << "The root of 25 is " << sqrt(value) << endl;
+    return 1;
+}
+
+int f2() {
+    cout << "\n";
+    cout << "   *   \n";
+    cout << "  ***  \n";
+    cout << " ***** \n";
+    cout << "*******\n";
+    cout << "*     *\n";
+    cout << "*     *\n";
+    cout << "*     *\n";
+    cout << "*******\n\n";
+    return 2;
+} 
+
+#pragma endregion
+
+const int ITEMS_NUMBER = 2;
+
+
+int main() {
+    CMenuItem items[ITEMS_NUMBER] {CMenuItem{"Root of 25", f1}, CMenuItem{"Print house", f2}};
+    CMenu menu("My console menu", items, ITEMS_NUMBER);
+    while (menu.runCommand()) {};
+
+    return 0;
+}
